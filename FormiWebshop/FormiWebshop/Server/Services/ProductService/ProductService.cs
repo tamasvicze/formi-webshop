b0bd8c7f962efbc26b630e4bb7b9c62eea72ce8c
Tamas Vicze <tamas.vicze@convenegroup.com>
2023-08-06 21:53:51 +0200

    Added product variant dropdown menu.

--- FormiWebshop/FormiWebshop/Server/Services/ProductService/ProductService.cs
+++ FormiWebshop/FormiWebshop/Server/Services/ProductService/ProductService.cs
@@ -9,20 +9,13 @@
             _context = context;
         }
 
-        public async Task<ServiceResponse<List<Product>>> GetProductListAsync()
-        {
-            var response = new ServiceResponse<List<Product>>
-            {
-                Data = await _context.Products.ToListAsync()
-            };
-
-            return response;
-        }
-
         public async Task<ServiceResponse<Product>> GetProductAsync(int productId)
         {
             var response = new ServiceResponse<Product>();
-            var product = await _context.Products.FindAsync(productId);
+            var product = await _context.Products
+                .Include(p => p.Variants)
+                .ThenInclude(v => v.ProductType)
+                .FirstOrDefaultAsync(p => p.Id == productId);
             if (product == null)
             {
                 response.Success = false;
@@ -36,12 +29,23 @@
             return response;
         }
 
+        public async Task<ServiceResponse<List<Product>>> GetProductListAsync()
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products.Include(p => p.Variants).ToListAsync()
+            };
+
+            return response;
+        }
+
         public async Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl)
         {
             var response = new ServiceResponse<List<Product>>
             {
                 Data = await _context.Products
                     .Where(p => p.Category.Url.ToLower().Equals(categoryUrl.ToLower()))
+                    .Include(p => p.Variants)
                     .ToListAsync()
             };
 
