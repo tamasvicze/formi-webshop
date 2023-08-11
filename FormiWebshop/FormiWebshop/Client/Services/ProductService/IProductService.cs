3d0d0ae13771385c8e1e251d0025982c58e040b2
Tamas Vicze <tamas.vicze@convenegroup.com>
2023-08-11 22:30:56 +0200

    Began adding pagination.

--- FormiWebshop/FormiWebshop/Client/Services/ProductService/IProductService.cs
+++ FormiWebshop/FormiWebshop/Client/Services/ProductService/IProductService.cs
@@ -7,6 +7,9 @@ namespace FormiWebshop.Client.Services.ProductService
         event Action ProductsChanged;
         List<Product> Products { get; set; } 
         string Message { get; set; }
+        int CurrentPage { get; set; }
+        int PageCount { get; set; }
+        string LastSearchText { get; set; }
         Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProduct(int productId);
         Task SearchProduct(string searchText);
--- FormiWebshop/FormiWebshop/Client/Services/ProductService/ProductService.cs
+++ FormiWebshop/FormiWebshop/Client/Services/ProductService/ProductService.cs
@@ -12,6 +12,9 @@
         public event Action? ProductsChanged;
         public List<Product> Products { get; set; } = new List<Product>();
         public string Message { get; set; } = "Loading products...";
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+        public string LastSearchText { get; set; } = string.Empty;
 
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
         {
@@ -29,6 +32,14 @@
                 Products = result.Data;
             }
 
+            CurrentPage = 1;
+            PageCount = 0;
+
+            if (Products.Count == 0)
+            {
+                Message = "No products found";
+            }
+
             ProductsChanged.Invoke();
         }
 
@@ -38,11 +49,15 @@
             return result.Data;
         }
 
-        public async Task SearchProduct(string searchText)
+        public async Task SearchProduct(string searchText, int page)
         {
-            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
-            if(result != null && result.Data != null)
-                Products = result.Data;
+            var result = await _http.GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
+            if (result != null && result.Data != null)
+            {
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
             if (Products.Count == 0) Message = "No products found.";
             ProductsChanged?.Invoke();
         }
