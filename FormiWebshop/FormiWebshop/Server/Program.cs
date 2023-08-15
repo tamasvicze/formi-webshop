1ccb20f7c7dbfcc4a352ec6658a32a42ae56eb03
Tamas Vicze <tamas.vicze@convenegroup.com>
2023-08-15 09:03:21 +0200

    User profile page and authentication middleware added.

--- FormiWebshop/FormiWebshop/Server/Program.cs
+++ FormiWebshop/FormiWebshop/Server/Program.cs
@@ -6,6 +6,8 @@ global using Microsoft.EntityFrameworkCore;
 global using FormiWebshop.Server.Services.CartService;
 global using FormiWebshop.Server.Services.AuthService;
 global using FormiWebshop.Server.Services.CategoryService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +27,19 @@ builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                    .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 var app = builder.Build();
 
@@ -50,6 +65,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorPages();
 app.MapControllers();
--- FormiWebshop/FormiWebshop/Server/Services/AuthService/AuthService.cs
+++ FormiWebshop/FormiWebshop/Server/Services/AuthService/AuthService.cs
@@ -1,7 +1,7 @@
-﻿using System.IdentityModel.Tokens.Jwt;
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
-using Microsoft.IdentityModel.Tokens;
 
 namespace FormiWebshop.Server.Services.AuthService
 {
@@ -16,51 +16,15 @@ namespace FormiWebshop.Server.Services.AuthService
             _configuration = configuration;
         }
 
-        public async Task<ServiceResponse<int>> Register(User user, string password)
-        {
-            if (await UserExists(user.Email))
-            {
-                return new ServiceResponse<int>
-                {
-                    Success = false,
-                    Message = "User already exists."
-                };
-            }
-
-            CreatePasswordHash(password, out byte[] passwordHash, out byte[] psswordSalt);
-
-            user.PasswordHash = passwordHash;
-            user.PasswordSalt = psswordSalt;
-
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return new ServiceResponse<int>
-            {
-                Data = user.Id,
-                Message = "Registration successful!"
-            };
-        }
-
-        public async Task<bool> UserExists(string email)
-        {
-            if (await _context.Users.AnyAsync(user => user.Email.ToLower()
-                    .Equals(email.ToLower())))
-            {
-                return true;
-            }
-            return false;
-        }
-
         public async Task<ServiceResponse<string>> Login(string email, string password)
         {
             var response = new ServiceResponse<string>();
-            var user = await _context.Users.
-                FirstOrDefaultAsync(user => user.Email.ToLower().Equals(email.ToLower()));
+            var user = await _context.Users
+                .FirstOrDefaultAsync(x => x.Email.ToLower().Equals(email.ToLower()));
             if (user == null)
             {
                 response.Success = false;
-                response.Message = "User not found";
+                response.Message = "User not found.";
             }
             else if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
             {
@@ -73,6 +37,38 @@ namespace FormiWebshop.Server.Services.AuthService
             }
 
             return response;
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            if (await UserExists(user.Email))
+            {
+                return new ServiceResponse<int>
+                {
+                    Success = false,
+                    Message = "User already exists."
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<int> { Data = user.Id, Message = "Registration successful!" };
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            if (await _context.Users.AnyAsync(user => user.Email.ToLower()
+                 .Equals(email.ToLower())))
+            {
+                return true;
+            }
+            return false;
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
@@ -104,18 +100,40 @@ namespace FormiWebshop.Server.Services.AuthService
             };
 
             var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8
-                .GetBytes(_configuration.GetSection("AppSettings:token").Value));
+                .GetBytes(_configuration.GetSection("AppSettings:Token").Value));
 
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
             var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: creds);
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds);
 
             var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
             return jwt;
+        }
+
+        public async Task<ServiceResponse<bool>> ChangePassword(int userId, string newPassword)
+        {
+            var user = await _context.Users.FindAsync(userId);
+            if (user == null)
+            {
+                return new ServiceResponse<bool>
+                {
+                    Success = false,
+                    Message = "User not found."
+                };
+            }
+
+            CreatePasswordHash(newPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.SaveChangesAsync();
+
+            return new ServiceResponse<bool> { Data = true, Message = "Password has been changed." };
         }
     }
 }
