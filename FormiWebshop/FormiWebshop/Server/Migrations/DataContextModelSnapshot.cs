99e253fd199205abaa50d07f26290d9cb8402759
Tamas Vicze <tamas.vicze@convenegroup.com>
2023-09-04 21:53:39 +0200

    Changed cart item added it to db.

--- FormiWebshop/FormiWebshop/Server/Migrations/DataContextModelSnapshot.cs
+++ FormiWebshop/FormiWebshop/Server/Migrations/DataContextModelSnapshot.cs
@@ -22,6 +22,25 @@ namespace FormiWebshop.Server.Migrations
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
 
+            modelBuilder.Entity("FormiWebshop.Shared.CartItem", b =>
+                {
+                    b.Property<int>("UserId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("ProductId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("ProductTypeId")
+                        .HasColumnType("int");
+
+                    b.Property<int>("Quantity")
+                        .HasColumnType("int");
+
+                    b.HasKey("UserId", "ProductId", "ProductTypeId");
+
+                    b.ToTable("CartItems");
+                });
+
             modelBuilder.Entity("FormiWebshop.Shared.Category", b =>
                 {
                     b.Property<int>("Id")
